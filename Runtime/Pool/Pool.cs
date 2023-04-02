bcbfabf771afdb507b1c97100ddc4f86a950c7b9
Andrew Murdoch <ajmurdoch96@gmail.com>
2023-04-02 13:34:00 +0100

    Rework Pool utility
    + removed pop/push actions, as this functionality can be perfomed at time of external pop/push
    + allow access to inactive/active lists
    + simplify extend functionality
    + destroy objects pushed into pool which do not originate from pool

--- Runtime/Pool/Pool.cs
+++ Runtime/Pool/Pool.cs
@@ -7,20 +7,20 @@ namespace VED.Utilities
     public class Pool<T> where T : MonoBehaviour
     {
         private T _original = null;
-        private Stack<T> _stack = new Stack<T>();
         private Transform _transform = null;
+
+        public Stack<T> Inactive => _inactive;
+        private Stack<T> _inactive = new Stack<T>();
 
         public List<T> Active => _active;
         private List<T> _active = new List<T>();
 
-        private Action<T> _onCreate = null;
-        private Action<T> _onDestroy = null;
-        private Action<T> _onPush = null;
-        private Action<T> _onPop = null;
+        private Action<T> _init   = null;
+        private Action<T> _deinit = null;
 
         private const int INITIAL_SIZE = 30;
 
-        public Pool(Transform parent, T original, int size = INITIAL_SIZE, Action<T> onCreate = null, Action<T> onDestroy = null, Action<T> onPush = null, Action<T> onPop = null)
+        public void Init(T original, Transform parent, int size = INITIAL_SIZE, Action<T> init = null, Action<T> deinit = null)
         {
             _original = original;
 
@@ -29,57 +29,65 @@ namespace VED.Utilities
             _transform.SetParent(parent);
             _transform.gameObject.SetActive(false);
 
-            _onCreate = onCreate;
-            _onDestroy = onDestroy;
-            _onPush = onPush;
-            _onPop = onPop;
+            _init = init;
+            _deinit = deinit;
 
             Extend(size);
         }
 
-        public void Destroy()
+        public void Deinit()
         {
-            int count = _stack.Count;
+            int count = _inactive.Count;
             for (int i = 0; i < count; i++)
             {
-                T instance = _stack.Pop();
-                _onDestroy?.Invoke(instance);
+                T instance = _inactive.Pop();
+                _deinit?.Invoke(instance);
                 UnityEngine.Object.Destroy(instance);
             }
 
-            _stack.Clear();
+            _inactive.Clear();
+            _active.Clear();
         }
 
         public T Pop()
         {
-            Extend();
+            T instance;
 
-            T instance = _stack.Pop();
-            instance.transform.SetParent(null);
+            if (_inactive.Count <= 0)
+            {
+                instance = UnityEngine.Object.Instantiate(_original);
+                _init?.Invoke(instance);
+                _active.Add(instance);
+                return instance;
+            }
+
+            instance = _inactive.Pop();
             _active.Add(instance);
-            _onPop?.Invoke(instance);
+            instance.transform.SetParent(null);
             return instance;
         }
 
         public void Push(T instance)
         {
-            if (_stack.Contains(instance)) return;
+            if (_inactive.Contains(instance)) return;
+            if (!_active.Contains(instance))
+            {
+                UnityEngine.Object.Destroy(instance.gameObject);
+                return;
+            }
 
             _active.Remove(instance);
-            _stack.Push(instance);
+            _inactive.Push(instance);
             instance.transform.SetParent(_transform);
-            _onPush?.Invoke(instance);
         }
 
         public void Extend(int size = 0)
         {
-            if (size == 0 && _stack.Count > 0) return;
-
-            size = Mathf.Max(1, size);
+            size = Mathf.Max(0, size);
             for (int i = 0; i < size; i++)
             {
                 T instance = UnityEngine.Object.Instantiate(_original);
-                _onCreate?.Invoke(instance);
+                _init?.Invoke(instance);
                 Push(instance);
             }
         }
