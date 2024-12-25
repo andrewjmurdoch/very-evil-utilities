e3ee0d124bdca4add69911782dcc6a417326bdff
Andrew Murdoch <ajmurdoch96@gmail.com>
2024-12-25 16:52:28 +0000

    Create state manager that can manage generic states

--- Runtime/Managers/StateManager/StateManager.cs
+++ Runtime/Managers/StateManager/StateManager.cs
@@ -29,7 +29,6 @@ namespace VED.Utilities
 
         public bool TryPeek(out State result)
         {
-            result = null;
             if (_stack.TryPeek(out result))
             {
                 return true; 
@@ -39,17 +38,71 @@ namespace VED.Utilities
 
         public void Tick()
         {
-            if (_stack.TryPeek(out State result)) result.Tick();
+            if (_stack.TryPeek(out State result))
+                result.Tick();
         }
 
         public void LateTick()
         {
-            if (_stack.TryPeek(out State result)) result.LateTick();
+            if (_stack.TryPeek(out State result))
+                result.LateTick();
         }
 
         public void FixedTick()
         {
-            if (_stack.TryPeek(out State result)) result.FixedTick();
+            if (_stack.TryPeek(out State result))
+                result.FixedTick();
+        }
+    }
+
+    public class StateManager<T> where T : State
+    {
+        public IReadOnlyCollection<T> Stack => _stack;
+        private Stack<T> _stack = new Stack<T>();
+
+        public Action<T> OnPush = null;
+        public Action<T> OnPop = null;
+
+        public void Push(T state)
+        {
+            _stack.Push(state);
+            OnPush?.Invoke(state);
+        }
+
+        public T Pop()
+        {
+            if (_stack.TryPop(out T result))
+            {
+                OnPop?.Invoke(result);
+                return result;
+            }
+            return null;
+        }
+
+        public bool TryPeek(out T result)
+        {
+            if (_stack.TryPeek(out result))
+                return true;
+            
+            return false;
+        }
+
+        public void Tick()
+        {
+            if (_stack.TryPeek(out T result))
+                result.Tick();
+        }
+
+        public void LateTick()
+        {
+            if (_stack.TryPeek(out T result))
+                result.LateTick();
+        }
+
+        public void FixedTick()
+        {
+            if (_stack.TryPeek(out T result))
+                result.FixedTick();
         }
     }
 }
