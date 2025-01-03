e82331f070b20923fadb88bab8a95b99cff1342e
Andrew Murdoch <ajmurdoch96@gmail.com>
2025-01-03 23:15:36 +0000

    Move mapper to correct namespace

--- Runtime/Mapper/Mapper.cs
+++ Runtime/Mapper/Mapper.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using VED.Utilities;
 
-namespace VED
+namespace VED.Utilities
 {
     [Serializable]
     public class Mapper<T, U>
