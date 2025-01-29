5742efaf70b36586360c2806ff2201df84d51f18
Andrew Murdoch <andrewjmurdoch@users.noreply.github.com>
2025-01-29 17:58:22 +0000

    Add vector lerps

--- Runtime/Easing/Easing.Lerp.cs
+++ Runtime/Easing/Easing.Lerp.cs
@@ -8,5 +8,15 @@ namespace VED.Utilities
         {
             return b + (a - b) * Mathf.Exp(-decayConstant * deltaTime);
         }
+
+        public static Vector2 Lerp(Vector2 a, Vector2 b, float deltaTime, float decayConstant)
+        {
+            return b + (a - b) * Mathf.Exp(-decayConstant * deltaTime);
+        }
+
+        public static Vector3 Lerp(Vector3 a, Vector3 b, float deltaTime, float decayConstant)
+        {
+            return b + (a - b) * Mathf.Exp(-decayConstant * deltaTime);
+        }
     }
 }
