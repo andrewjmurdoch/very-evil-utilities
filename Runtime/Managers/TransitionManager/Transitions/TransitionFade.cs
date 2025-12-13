2669fa8982b2f17b2c18008b58b0d7ecf1a84ad7
Andrew Murdoch <ajmurdoch96@gmail.com>
2025-12-13 16:59:09 +0000

    Swap out image in TransitionFade example

--- Runtime/Managers/TransitionManager/Transitions/TransitionFade.cs
+++ Runtime/Managers/TransitionManager/Transitions/TransitionFade.cs
@@ -6,52 +6,50 @@ namespace VED.Utilities
 {
     public class TransitionFade : Transition
     {
-        private TimerRealtime _timer = null;
+        private TimerRealtime _timer = new(DURATION);
 
         private const float ALPHA_MIN = 000.000f;
         private const float ALPHA_MAX = 001.000f;
         private const float DURATION  = 000.500f;
 
-        [SerializeField] private RawImage _rawImage = null;
+        [SerializeField] private Image _image = null;
 
         [SerializeField] private Easing.Controller _easingController = new Easing.Controller(Easing.Shape.SINE, Easing.Extent.INOUT);
 
-        protected override void Awake()
-        {
-            base.Awake();
-            _timer = new TimerRealtime(DURATION);
-        }
-
         public override void Stop()
         {
-            _rawImage.color = Color.clear;
+            _image.color = Color.clear;
             _timer.Reset();
         }
 
         public override void In(Action callback = null)
         {
-            _rawImage.color = Color.black;
-            void Update() => _rawImage.color = Color.black * new Color(1f, 1f, 1f, _easingController.Ease(ALPHA_MIN, ALPHA_MAX, _timer.InverseElapsed));
-            callback += () => _rawImage.color = Color.clear;
+            _image.color = Color.black;
+
+            void Update() => _image.color = Color.black * new Color(1f, 1f, 1f, _easingController.Ease(ALPHA_MIN, ALPHA_MAX, _timer.InverseElapsed));
+            callback += () => _image.color = Color.clear;
+
             _timer.Set(Update, callback);
         }
 
         public override void Out(Action callback = null)
         {
-            _rawImage.color = Color.clear;
-            void Update() => _rawImage.color = Color.black * new Color(1f, 1f, 1f, _easingController.Ease(ALPHA_MIN, ALPHA_MAX, _timer.Elapsed));
-            callback += () => _rawImage.color = Color.black;
+            _image.color = Color.clear;
+
+            void Update() => _image.color = Color.black * new Color(1f, 1f, 1f, _easingController.Ease(ALPHA_MIN, ALPHA_MAX, _timer.Elapsed));
+            callback += () => _image.color = Color.black;
+
             _timer.Set(Update, callback);
         }
 
         public override void SetIn()
         {
-            _rawImage.color = Color.clear;
+            _image.color = Color.clear;
         }
 
         public override void SetOut()
         {
-            _rawImage.color = Color.black;
+            _image.color = Color.black;
         }
     }
 }
