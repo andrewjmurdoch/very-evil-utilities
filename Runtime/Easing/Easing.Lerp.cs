e43fb2a8a44e4ea19c15e07e300d7ac092addbf2
Andrew Murdoch <ajmurdoch96@gmail.com>
2025-12-26 10:08:58 +0000

    Add default delta time lerp functions

--- Runtime/Easing/Easing.Lerp.cs
+++ Runtime/Easing/Easing.Lerp.cs
@@ -9,9 +9,19 @@ namespace VED.Utilities
             return b + (a - b) * Mathf.Exp(-decayConstant * deltaTime);
         }
 
+        public static float Lerp(float a, float b, float decayConstant)
+        {
+            return b + (a - b) * Mathf.Exp(-decayConstant * Time.deltaTime);
+        }
+
         public static Vector2 Lerp(Vector2 a, Vector2 b, float deltaTime, float decayConstant)
         {
             return b + (a - b) * Mathf.Exp(-decayConstant * deltaTime);
+        }
+
+        public static Vector2 Lerp(Vector2 a, Vector2 b, float decayConstant)
+        {
+            return b + (a - b) * Mathf.Exp(-decayConstant * Time.deltaTime);
         }
 
         public static Vector3 Lerp(Vector3 a, Vector3 b, float deltaTime, float decayConstant)
@@ -19,9 +29,19 @@ namespace VED.Utilities
             return b + (a - b) * Mathf.Exp(-decayConstant * deltaTime);
         }
 
+        public static Vector3 Lerp(Vector3 a, Vector3 b, float decayConstant)
+        {
+            return b + (a - b) * Mathf.Exp(-decayConstant * Time.deltaTime);
+        }
+
         public static Color Lerp(Color a, Color b, float deltaTime, float decayConstant)
         {
             return b + (a - b) * Mathf.Exp(-decayConstant * deltaTime);
+        }
+
+        public static Color Lerp(Color a, Color b, float decayConstant)
+        {
+            return b + (a - b) * Mathf.Exp(-decayConstant * Time.deltaTime);
         }
     }
 }
