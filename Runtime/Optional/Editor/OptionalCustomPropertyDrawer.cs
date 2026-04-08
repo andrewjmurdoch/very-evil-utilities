1cf5abdd398a00b355d48c9bd6cb75676fb37a8d
Andrew Murdoch <andrewjmurdoch@users.noreply.github.com>
2026-04-08 16:20:18 +0100

    Add TextArea style property drawer on Optional<string> fields

--- Runtime/Optional/Editor/OptionalCustomPropertyDrawer.cs
+++ Runtime/Optional/Editor/OptionalCustomPropertyDrawer.cs
@@ -15,13 +15,26 @@ namespace VED.Utilities
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            SerializedProperty valueProperty = property.FindPropertyRelative("Value");
+            SerializedProperty valueProperty   = property.FindPropertyRelative("Value");
             SerializedProperty enabledProperty = property.FindPropertyRelative("Enabled");
 
             EditorGUI.BeginProperty(position, label, property);
                 position.width -= 24;
                 EditorGUI.BeginDisabledGroup(!enabledProperty.boolValue);
+                
+                if (   valueProperty.propertyType == SerializedPropertyType.String 
+                    && valueProperty.stringValue.Length > 20)
+                {
+                    EditorGUI.LabelField(position, label);
+                    GUIStyle style = new(EditorStyles.textArea) { wordWrap = true };
+                    float height = EditorGUIUtility.singleLineHeight * 3f;
+                    valueProperty.stringValue = EditorGUILayout.TextArea(valueProperty.stringValue, style, GUILayout.Height(height));
+                }
+                else
+                {
                     EditorGUI.PropertyField(position, valueProperty, label, true);
+                }
+                
                 EditorGUI.EndDisabledGroup();
 
                 int indent = EditorGUI.indentLevel;
