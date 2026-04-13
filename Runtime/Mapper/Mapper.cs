f95cb455d9c903f89ca250c4bc5950d909af0f08
Andrew Murdoch <ajmurdoch96@gmail.com>
2026-04-13 21:47:46 +0100

    Create Mapper constructor with list of maps

--- Runtime/Mapper/Mapper.cs
+++ Runtime/Mapper/Mapper.cs
@@ -12,7 +12,7 @@ namespace VED.Utilities
     
         public Map(T key, U value)
         {
-            Key = key;
+            Key   = key;
             Value = value;
         }
     }
@@ -26,6 +26,16 @@ namespace VED.Utilities
         private Dictionary<T, U>  Dictionary => _dictionary ??= InitDictionary();
         private Dictionary<T, U> _dictionary = null;
     
+        public Mapper()
+        {
+
+        }
+
+        public Mapper(List<Map<T, U>> maps)
+        {
+            _maps = maps;
+        }
+
         private Dictionary<T, U> InitDictionary()
         {
             _dictionary = new Dictionary<T, U>();
@@ -43,7 +53,9 @@ namespace VED.Utilities
         {
             get
             {
-                if (Get(key, out U value)) return value;
+                if (Get(key, out U value))
+                    return value;
+
                 return default(U);
             }
     
@@ -71,7 +83,9 @@ namespace VED.Utilities
         {
             foreach (Map<T, U> map in _maps)
             {
-                if (!map.Key.Equals(key)) continue;
+                if (!map.Key.Equals(key))
+                    continue;
+
                 map.Value = value;
                 Validate();
                 return true;
@@ -89,9 +103,14 @@ namespace VED.Utilities
     
                 for (int j = i - 1; j >= 0; j--)
                 {
-                    if (_maps[j].Key == null) continue;
-                    if (!_maps[i].Key.Equals(_maps[j].Key)) continue;
-                    if (i == _maps.Count - 1 && j == i - 1) continue;
+                    if (_maps[j].Key == null) 
+                        continue;
+
+                    if (!_maps[i].Key.Equals(_maps[j].Key))
+                        continue;
+
+                    if (i == _maps.Count - 1 && j == i - 1)
+                        continue;
     
                     _maps.RemoveAt(i);
                     break;
@@ -101,14 +120,17 @@ namespace VED.Utilities
             Dictionary.Clear();
             foreach (Map<T, U> map in _maps)
             {
-                if (map.Key == null) continue;
+                if (map.Key == null) 
+                    continue;
+
                 Dictionary.TryAdd(map.Key, map.Value);
             }
         }
     
         public bool Add(T key, U value)
         {
-            if (Dictionary.ContainsKey(key)) return false;
+            if (Dictionary.ContainsKey(key))
+                return false;
     
             _maps.Add(new Map<T, U>(key, value));
             Validate();
@@ -122,7 +144,8 @@ namespace VED.Utilities
     
             for (int i = _maps.Count - 1; i >= 0; i--)
             {
-                if (!_maps[i].Key.Equals(key)) continue;
+                if (!_maps[i].Key.Equals(key))
+                    continue;
     
                 _maps.RemoveAt(i);
                 Validate();
@@ -142,6 +165,7 @@ namespace VED.Utilities
 
             if (_dictionary != null)
                 _dictionary.Clear();
+
             _dictionary = null;
         }
     }
